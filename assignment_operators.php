94a7ce26159dcc9c31c4978101b17731da61b5e4
YadBro <ncada708@gmail.com>
2022-10-18 22:41:01 +0700

    assignment operators

---
+++ assignment_operators.php
@@ -0,0 +1,13 @@
+<?php
+$num1 = 1;
+$num2 = 2;
+
+$num1 = $num1 + $num2;
+$num1 = $num1 - $num2;
+$num1 = $num1 * $num2;
+$num1 = $num1 / $num2;
+
+$num1 += $num2;
+$num1 -= $num2;
+$num1 *= $num2;
+$num1 /= $num2;

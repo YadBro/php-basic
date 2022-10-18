ae5a7b50ccae17c6ce0d20d862e186bd368f7a92
YadBro <ncada708@gmail.com>
2022-10-19 05:53:37 +0700

    form & handling

--- form/form_handling.php
+++ form/form_handling.php
@@ -1,10 +1,15 @@
 <?php
-$name = $_POST['name'];
-$age = $_GET['age'];
 // Simplenya get dan post. kalo get data formnya ada di url kalo post itu di hide
 
-var_dump($name);
-echo '<br>';
-var_dump($age);
-echo '<br>';
-echo 'REQUEST METHOD is: ' . $_SERVER['REQUEST_METHOD'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = htmlspecialchars($_POST['name']); // htmlspecialchars menghindari script html aneh yang merusak website kita
+  var_dump($name);
+  echo 'REQUEST METHOD is: ' . $_SERVER['REQUEST_METHOD'];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $age = $_GET['age']; // htmlspecialchars menghindari script html aneh yang merusak website kita
+  var_dump($age);
+  echo 'REQUEST METHOD is: ' . $_SERVER['REQUEST_METHOD'];
+}

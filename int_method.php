26990a0e2d996eabc6503946745a868a4a260a06
YadBro <ncada708@gmail.com>
2022-10-18 17:42:03 +0700

    integer method

---
+++ int_method.php
@@ -0,0 +1,13 @@
+<?php
+$number1 = 1;
+$number1 = 'a';
+
+
+echo 'max integer in php: ' . PHP_INT_MAX;
+echo '<br />';
+echo 'min integer in php: ' . PHP_INT_MIN;
+echo '<br />';
+echo 'size integer in php: ' . PHP_INT_SIZE;
+
+echo '<br />';
+var_dump(is_int($number1));

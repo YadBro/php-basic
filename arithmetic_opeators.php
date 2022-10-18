128e4b3f79bf3d140aa81caeb300068e25a93c42
YadBro <ncada708@gmail.com>
2022-10-18 20:50:02 +0700

    arithmetic operators

---
+++ arithmetic_opeators.php
@@ -0,0 +1,28 @@
+<?php
+
+$num1 = 29;
+$num2 = 40;
+
+// addition
+echo '<br />';
+echo "$num1 + $num2 = " . $num1 + $num2;
+
+// subtraction
+echo '<br />';
+echo "$num1 - $num2 = " . $num1 - $num2;
+
+// multiplication
+echo '<br />';
+echo "$num1 * $num2 = " . $num1 * $num2;
+
+// Division
+echo '<br />';
+echo "$num1 / $num2 = " . $num1 / $num2;
+
+// Modulus
+echo '<br />';
+echo "$num1 % $num2 = " . $num1 % $num2;
+
+// Exponentiation
+echo '<br />';
+echo "$num1 ** $num2 = " . $num1 ** $num2;

e7da77522a31c455b31b37e0492a8dcb5dc22555
YadBro <ncada708@gmail.com>
2022-10-18 22:47:14 +0700

    comparasion operators

---
+++ comparasion_operators.php
@@ -0,0 +1,24 @@
+<?php
+
+$word1 = 'hello';
+$word2 = 'hellos';
+
+echo "$word1 == $word2 " . var_dump($word1 == $word2);
+echo '<br />';
+echo "$word1 === $word2 " . var_dump($word1 === $word2);
+echo '<br />';
+echo "$word1 != $word2 " . var_dump($word1 != $word2);
+echo '<br />';
+echo "$word1 !== $word2 " . var_dump($word1 !== $word2);
+echo '<br />';
+echo "$word1 <> $word2 " . var_dump($word1 <> $word2);
+echo '<br />';
+echo "$word1 > $word2 " . var_dump($word1 > $word2);
+echo '<br />';
+echo "$word1 < $word2 " . var_dump($word1 < $word2);
+echo '<br />';
+echo "$word1 >= $word2 " . var_dump($word1 >= $word2);
+echo '<br />';
+echo "$word1 <= $word2 " . var_dump($word1 <= $word2);
+echo '<br />';
+echo "$word1 <=> $word2 " . var_dump($word1 <=> $word2);

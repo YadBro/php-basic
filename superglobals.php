3afb7a975c9e6556df5e964e36766a1210956e37
YadBro <ncada708@gmail.com>
2022-10-19 05:13:33 +0700

    superglobals

---
+++ superglobals.php
@@ -0,0 +1,35 @@
+<?php
+
+echo $_SERVER['PHP_SELF'];
+echo '<br />';
+echo $_SERVER['GATEWAY_INTERFACE'];
+echo '<br />';
+echo $_SERVER['SERVER_ADDR'];
+echo '<br />';
+echo $_SERVER['SERVER_NAME'];
+echo '<br />';
+echo $_SERVER['SERVER_SOFTWARE'];
+echo '<br />';
+echo $_SERVER['SERVER_PROTOCOL'];
+echo '<br />';
+echo $_SERVER['REQUEST_METHOD'];
+echo '<br />';
+echo $_SERVER['QUERY_STRING'];
+echo '<br />';
+echo $_SERVER['HTTP_ACCEPT'];
+echo '<br />';
+echo $_SERVER['HTTP_HOST'];
+echo '<br />';
+echo $_SERVER['REMOTE_ADDR'];
+echo '<br />';
+echo $_SERVER['REMOTE_PORT'];
+echo '<br />';
+echo $_SERVER['SCRIPT_FILENAME'];
+echo '<br />';
+echo $_SERVER['SERVER_ADMIN'];
+echo '<br />';
+echo $_SERVER['SERVER_PORT'];
+echo '<br />';
+echo $_SERVER['SERVER_SIGNATURE'];
+echo '<br />';
+echo $_SERVER['SCRIPT_NAME'];

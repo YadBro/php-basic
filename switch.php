7ce3add6b68c660a7085ef7f3d1d07f0e8afe596
YadBro <ncada708@gmail.com>
2022-10-18 22:59:04 +0700

    switch

---
+++ switch.php
@@ -0,0 +1,13 @@
+<?php
+$color = 'red';
+
+switch ($color) {
+  case 'red';
+    echo 'you are red';
+    break;
+  case 'green';
+    echo 'you are green';
+    break;
+  default:
+    echo 'I don\'t know that color :(';
+}

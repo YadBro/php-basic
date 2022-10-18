4cb04676a7bbc670f41784102b9f319419dac2e9
YadBro <ncada708@gmail.com>
2022-10-19 05:12:47 +0700

    argument by reference

--- constant_variable.php
+++ constant_variable.php
@@ -8,6 +8,28 @@
 define('GREETING', 'Welcome to wkwkland');
 echo GREETING;
 
+
+define('num2', 200);
+const num1 = 5;
+$num2 = 1;
+echo '<br />';
+echo num1;
+
+function testGlobal()
+{
+  global $num2;
+  echo '<br />';
+  num1 + 7;
+  num2 + 1;
+  echo num1;
+  echo '<br />';
+  echo num2;
+  echo '<br />';
+  echo $num2;
+}
+
+testGlobal();
+
 echo '<br />';
 define('sayhi', 'Hi Yadi Apriyadi');
 echo sayhi;
